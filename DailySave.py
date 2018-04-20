9b1736b0482049dbf3c0f8301afa841e380ed8a7
real0day <CMagistrado@gmail.com>
2018-04-20 10:25:08 -0700

    Used for generating new blacklist files daily

---
+++ DailySave.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+class RotatingFileOpener():
+    def __init__(self, path, mode='a', prepend="", append=""):
+        if not os.path.isdir(path):
+            raise FileNotFoundError("Can't open directory '{}' for data output.".format(path))
+        self._path = path
+        self._prepend = prepend
+        self._append = append
+        self._mode = mode
+        self._day = time.localtime().tm_mday
+    def __enter__(self):
+        self._filename = self._format_filename()
+        self._file = open(self._filename, self._mode)
+        return self
+    def __exit__(self, *args):
+        return getattr(self._file, '__exit__')(*args)
+    def _day_changed(self):
+        return self._day != time.localtime().tm_mday
+    def _format_filename(self):
+        return os.path.join(self._path, "{}{}{}".format(self._prepend, time.strftime("%Y%m%d"), self._append))
+    def write(self, *args):
+        if self._day_changed():
+            self._file.close()
+            self._file = open(self._format_filename())
+        return getattr(self._file, 'write')(*args)
+    def __getattr__(self, attr):
+        return getattr(self._file, attr)
+    def __iter__(self):
+        return iter(self._file)
